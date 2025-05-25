701c48fed89b4c69ee87451027726545e4d1346e
Daniel <da.aldanag@gmail.com>
2025-05-25 11:04:54 -0600

    Daniel Aldana: creacion de clase de grafos y generador de grafos por texto

--- src/main/java/models/Edge.java
+++ src/main/java/models/Edge.java
@@ -1,21 +1,28 @@
 package models;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
 public class Edge {
     private double distance;
-    private double time;
-    private int toNodeId;
+    private Map<Integer, Double> timeByHour;
+    private Node toNode;
 
-    public Edge(double distance, double time, int toNodeId) {
+    public Edge(double distance, Node toNode) {
         this.distance = distance;
-        this.time = time;
-        this.toNodeId = toNodeId;
+        this.timeByHour = new HashMap<>();
+        this.toNode = toNode;
     }
 
-    public int getToNodeId() { return toNodeId; }
     public double getDistance() { return distance; }
-    public double getTime() { return time; }
+    public Node getToNode() { return toNode; }
+    public double getTimeByHour(int hour) { return timeByHour.getOrDefault(hour, distance / 30); }
 
     public void setDistance(double distance) { this.distance = distance; }
-    public void setTime(double time) { this.time = time; }
-    public void setToNodeId(int toNodeId) { this.toNodeId = toNodeId; }
+    public void setToNode(Node toNode) { this.toNode = toNode; }
+    public void setTimeByHour(int hour, double time) { timeByHour.put(hour, time); }
+
+    @Override
+    public int hashCode() { return Objects.hash(toNode); }
 }
