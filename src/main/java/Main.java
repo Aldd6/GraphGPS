f53fec7125da2d3e753bc5a28b7ed32facba53e5
Daniel <da.aldanag@gmail.com>
2025-05-25 12:30:36 -0600

    Daniel Aldana: creacion de metodo de dijkstra en funcion del tiempo y grafo de prueba

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,11 @@
+import models.DirectedGraph;
 import models.GraphConstructor;
 
 public class Main {
     public static void main(String[] args) {
         try {
-            GraphConstructor.loadGraph("C:\\Users\\DELL\\Desktop\\grafo.txt");
+            DirectedGraph graph = GraphConstructor.loadGraph("src/main/resources/grafo_tigo_antigua_ubicaciones.txt");
+            System.out.println(graph.dijkstra(graph.getNode(1),graph.getNode(8),5));
         }catch (Exception e){
             e.printStackTrace();
         }
